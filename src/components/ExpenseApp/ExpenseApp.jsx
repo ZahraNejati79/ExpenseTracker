3feeebdc1800fe0d5e5203a130507c694369d9c3
ZahraNejati <nejatizahra4@gmail.com>
2021-10-17 11:21:17 +0330

    set income&expense value

--- src/components/ExpenseApp/ExpenseApp.jsx
+++ src/components/ExpenseApp/ExpenseApp.jsx
@@ -1,23 +1,38 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./ExpenseApp.module.css";
 import OverView from "../OverView/OverView";
 import TransAction from "../TransAction/TransAction";
 
 const ExpenseApp = () => {
   const [expense, setSpense] = useState(0);
-  const [incom, setIncome] = useState(0);
+  const [income, setIncome] = useState(0);
   const [transaction, setTransaction] = useState([]);
+
   const addTarnsAction = (formValues) => {
     console.log(formValues);
-    const data = { ...formValues, id: Date.now };
+    const data = { ...formValues, id: Date.now() };
     setTransaction([...transaction, data]);
   };
+  useEffect(() => {
+    let exp = 0;
+    let inc = 0;
+
+    transaction.forEach((t) => {
+      t.type === "expense"
+        ? (exp = exp + parseFloat(t.amount))
+        : (inc = inc + parseFloat(t.amount));
+    });
+
+    setIncome(inc);
+    setSpense(exp);
+  }, [transaction]);
+
   return (
     <div className={styles.container}>
       <div>
         <OverView
           expense={expense}
-          incom={incom}
+          income={income}
           addTarnsAction={addTarnsAction}
         />
       </div>
