a4ce7761b32361c23ee5d589c85b338a1bb9029f
ZahraNejati <nejatizahra4@gmail.com>
2021-10-24 11:06:19 +0330

    search

--- src/components/TransAction/TransAction.jsx
+++ src/components/TransAction/TransAction.jsx
@@ -1,17 +1,49 @@
+import { useEffect, useState } from "react";
 import styles from "./TransAction.module.css";
 const TransAction = ({ transaction }) => {
+  const [search, setSearch] = useState("");
+  const [filterTr, setFilterTn] = useState(transaction);
+
+  const filterTransArtion = (search) => {
+    if (!search || search === "") {
+      setFilterTn(transaction);
+    } else {
+      const filtered = transaction.filter((tra) =>
+        tra.desc.toLowerCase().includes(search)
+      );
+      setFilterTn(filtered);
+    }
+  };
+  const changeHandler = (e) => {
+    setSearch(e.target.value);
+    filterTransArtion(e.target.value);
+  };
+  useEffect(() => {
+    filterTransArtion(search);
+  }, [transaction]);
+
+  if (!transaction.length) return <h3>Add TransAction</h3>;
   return (
     <div>
-      {transaction.map((t) => (
-        <div
-          className={styles.transAction}
-          key={t.id}
-          style={{ borderRight: t.type === "expense" && "3px solid red" }}
-        >
-          <span>{t.desc}</span>
-          <span>$ {t.amount}</span>
-        </div>
-      ))}
+      <input
+        type="text"
+        value={search}
+        onChange={changeHandler}
+        placeholder=" Search"
+        className={styles.search}
+      />
+      {filterTr.length
+        ? filterTr.map((t) => (
+            <div
+              className={styles.transAction}
+              key={t.id}
+              style={{ borderRight: t.type === "expense" && "3px solid red" }}
+            >
+              <span>{t.desc}</span>
+              <span>$ {t.amount}</span>
+            </div>
+          ))
+        : "Add TransAction"}
     </div>
   );
 };
